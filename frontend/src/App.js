047332ed2a52978ce5cd20af4e04e4d39b8c118c
Selvakumar S <2002selvakumar@gmail.com>
2024-09-17 14:01:16 +0530

    JSX & JavaScript Expressions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import SecondComponent from './Components/SecondComponent';
+import React from 'react';
 
 function App() {
   function MyComponent(){
-    return <h1>My FIRST component</h1>
+    return (
+      <>
+        <h1 className='style1'>React.Fragment</h1>
+        {/* can create this 'style1' in either index.css file or App.css file.. im using index.css file */}
+        <div>
+          <h1>My FIRST component</h1>
+          <p>Part of MyFirstComponent</p>
+        </div>
+      </>
+    ) 
   }
   return (
     <div>
--- frontend/src/Components/SecondComponent.js
+++ frontend/src/Components/SecondComponent.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function SecondComponent (){
-    return <h1>Second Component</h1>
+    function AppleDisplay(n) {
+        if (n === 0 || n === 1) {
+            return `john has ${n} apple`;
+        } else if (n > 1) {
+            return `john has ${n} apples`;
+        }
+        else{
+            return `john has ${Math.abs(n)} apples`;
+        }
+    }    
+    return (
+        <>
+        <h1>Second Component</h1>
+        <h2>john has {2**4} apples </h2>
+        <h3>{AppleDisplay(-6)}</h3>
+        {/* JSX is very intuitive so we can use {} and write expressions as in js */}
+        </>
+    )
 }
 
 export default SecondComponent;
